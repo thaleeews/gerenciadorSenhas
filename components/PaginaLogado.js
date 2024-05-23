94b3520c343574acb9a3316edcdddb7e4a24e466
Thales Pasquotto <thales_clemente@outlook.com>
2024-05-23 20:27:56 -0300

    Update PaginaLogado.js

--- components/PaginaLogado.js
+++ components/PaginaLogado.js
@@ -112,6 +112,7 @@ const styles = StyleSheet.create({
         borderColor: 'gray',
         borderWidth: 1,
         borderRadius: 5,
+        backgroundColor: 'mistyrose'
     },
     senhaText: {
         fontSize: 16,
